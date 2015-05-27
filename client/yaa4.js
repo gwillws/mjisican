76e6a9dcae1b7cd5ff10cd51e3758e22bedb6687
mjisicacn <info.gwillws@gmail.com>
2015-05-27 18:41:34 +0800

    commited

---
+++ client/yaa4.js
@@ -0,0 +1,20 @@
+//yaa = new Mongo.Collection('yaa');
+
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+ 
+  });
+
+  Template.hello.events({
+
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
